34869e19993a2225b91f4787bd8ea07c0b8e66b9
amartincastro <castroarielm@gmail.com>
2019-10-08 13:19:24 -0700

    property binding for server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
 export class ServerComponent {
     serverId: number = 10;
     serverStatus = 'offline';
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
 
