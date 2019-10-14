6598253cbe835d55c5b3ef3e0bf1b07bb6d2493f
amartincastro <castroarielm@gmail.com>
2019-10-14 14:34:17 -0700

    maintain newly added servers in state

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created';
   serverName = 'test test';
   serverCreated = false;
+  servers =['Testserver', 'Testserver 2'];
 
   constructor() { 
     setTimeout(() => {
@@ -24,6 +25,7 @@ export class ServersComponent implements OnInit {
   onCreateServer() {
     this.serverCreated = true;
     this.serverCreationStatus = 'Server was created. Name is ' + this.serverName;
+    this.servers.push(this.serverName);
   }
 
   onUpdateServerName(event: Event) { 
